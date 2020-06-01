d3cbd8a29b32b360411933c752b730878627cb49
jigius <jigius@gmail.com>
2020-06-01 09:25:20 +0700

    fix

---
+++ src/Inventory/Asset/Stub.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the jigius/acc-core library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) 2020 Jigius <jigius@gmail.com>
+ * @link https://github.com/jigius/acc-core GitHub
+ */
+
+declare(strict_types=1);
+
+namespace Acc\Core\Inventory\Asset;
+
+use Acc\Core\Inventory\AssetInterface;
+use LogicException;
+
+/**
+ * Class Stub
+ * @package Acc\Core\Inventory\Asset
+ */
+class Stub implements AssetInterface
+{
+    /**
+     * @inheritDoc
+     * @throws LogicException
+     */
+    public function serialized(): array
+    {
+        throw new LogicException("is not implemented yet");
+    }
+
+    /**
+     * @inheritDoc
+     * @throws LogicException
+     */
+    public function unserialized(array $data): AssetInterface
+    {
+        throw new LogicException("is not implemented yet");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function test($val): void
+    {
+        // nothing to do - this is just a stub
+        return;
+    }
+}
